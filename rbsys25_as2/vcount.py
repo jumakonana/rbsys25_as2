fd61feb3e328d5aa9cb753d778e6730d1ef8e317
jun konann <s24C1079LG@s.chibakoudai.jp>
2026-01-03 16:37:13 +0900

    class

--- rbsys25_as2/vcount.py
+++ rbsys25_as2/vcount.py
@@ -3,11 +3,19 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 rclpy.init()
-node = Node("vcount")
 
 
-def cb():
+class Vcount(Node):
+    def __init__(self):
+        super().__init__('vcount')
+        
+        self.declare_parameter("deadline", 10)
+        
+        self.create_timer(0.5, self.cb)
 
+    def cb(self):
+        deadline = self.get_parameter("deadline").value
 
 def main():
+    node = Vcount()
     rclpy.spin(node)
