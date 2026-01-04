711b5540ef25297e2d2838c4ff91824b382acd16
jun konann <s24C1079LG@s.chibakoudai.jp>
2026-01-04 15:10:06 +0900

    clear count

--- rbsys25_as2/vcount.py
+++ rbsys25_as2/vcount.py
@@ -9,25 +9,23 @@ class Vcount(Node):
     def __init__(self):
         super().__init__('vcount')
         
-        global pub
-
         self.declare_parameter("deadline", 10)
         
+        self.deadline = self.get_parameter("deadline").value
 
-        pub = self.create_publisher(Int16, "countup", 10)
-        #self.create_timer(0.5, self.cb)
+        self.pub = self.create_publisher(Int16, "countup", 10)
+        self.create_timer(1, self.cb)
+        
+    def cb(self):   
 
-    def cb(self):
-        global n
-        deadline = self.get_parameter("deadline").value
-        deadline += 1
-        self.get_logger().info(f"{deadline}")
-        n = deadline
+        self.get_logger().info(f"{self.deadline}")
+        self.deadline += 1
        
-        self.create_timer(0.5, self.cb)
+        
+  
         msg = Int16()
-        msg.data = n
-        pub.publish(msg)
+        msg.data = self.deadline
+        self.pub.publish(msg)
 
 def main():
     node = Vcount()
